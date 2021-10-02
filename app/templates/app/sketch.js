032438874fbd1a68e7e7ab467d6a1e0261bc9c93
alejandroxpx <63479498+alejandroxpx@users.noreply.github.com>
2021-10-02 12:43:00 -0500

    first commit

--- app/templates/app/sketch.js
+++
@@ -1,9 +0,0 @@
-function setup(){
-    createCanvas(200,200);
-}
-
-function draw() {
-    background(200,0,200);
-
-    rect(100,50,25,75,80);
-}
--- manage.py
+++ manage.py
@@ -16,6 +16,7 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
+    print("test")
 
 
 if __name__ == '__main__':
